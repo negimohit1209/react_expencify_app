36dde5ab127a28afce0420bee6130ca1cbb2f5eb
negimohit1209 <mohitsngh1997@gmail.com>
2018-08-25 14:11:36 +0530

    remove dummy daat

--- src/index.js
+++ src/index.js
@@ -10,33 +10,6 @@ import getVisibleExpenses from './selectors/expences';
 
 const store = configureStore();
 
-store.dispatch(
-    addExpence({
-        description: "water bill",
-        amount: 500,
-        createdAt: -11000
-      })
-);
-store.dispatch(
-    addExpence({
-        description: "phone bill",
-        amount: 100,
-      createdAt: 1000
-      })
-);
-store.dispatch(
-    addExpence({
-        description: "electricity bill",
-        amount: 1200,
-      createdAt: 100
-      })
-); 
-console.log(store.getState());
-const state = store.getState();
-const visibleExpence = getVisibleExpenses(state.expences, state.filters);
-console.log(visibleExpence);
-
-// console.log(getVisibleExpenses(store.state.expences, store.state.filters));
 const jsx = (
     <Provider store={store}>
     <AppRouter />
