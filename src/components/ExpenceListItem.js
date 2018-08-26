40d0541e7dac392b53dbdc2ac5105a110df9f2fe
negimohit1209 <mohitsngh1997@gmail.com>
2018-08-26 14:19:52 +0530

    setup formating amount and createSt

--- src/components/ExpenceListItem.js
+++ src/components/ExpenceListItem.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
-const ExpenceListItem = ( { id ,description, amount, createdAt }) => {
+import React from "react";
+import { Link } from "react-router-dom";
+import moment from 'moment';
+import numeral from 'numeral';
+const ExpenceListItem = ({ id, description, amount, createdAt }) => {
   return (
     <div>
-    <Link to={`/edit/${id}`}>
-    <h3>{description}</h3>
-    </Link>
-        
-        <p>{amount} - {createdAt}</p>
-        
+      <Link to={`/edit/${id}`}>
+        <h3>{description}</h3>
+      </Link>
+
+      <p>
+        {numeral(amount / 100).format('$0,0.00')} - {moment(createdAt).format('MMMM Do, YYYY')}
+      </p>
     </div>
-  )
-}
+  );
+};
 
 export default ExpenceListItem;
